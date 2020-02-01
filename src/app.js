fbbcc882069caa814681d30f24e5ebc255b31fbc
Daniel Di Venere <danieldivenere@gmail.com>
2020-02-01 15:58:38 -0800

    fixed errors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
 app.use((error, req, res, next) => {
   if (NODE_ENV === 'production') {
     response = {
@@ -28,6 +29,15 @@ app.use((error, req, res, next) => {
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Got, world!');
+});
+
+app.post('/user', (req, res) => {
+  const { username, password, favoriteClub, newsLetter } = req.body;
 });
 
 module.exports = app;
